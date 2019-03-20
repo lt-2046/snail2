eba9032955d16297539372ad6c82ddba679ead82
snail <lt-2046@163.com>
2019-03-20 15:15:59 +0800

    EC缓存修改

--- snail-web/src/main/java/com/snail/shiro/filter/MyShiroPermFilter.java
+++ snail-web/src/main/java/com/snail/shiro/filter/MyShiroPermFilter.java
@@ -12,6 +12,14 @@ import javax.servlet.http.HttpServletRequest;
  */
 public class MyShiroPermFilter extends AccessControlFilter {
 
+    /**
+     * 表示是否允许访问；mappedValue就是[urls]配置中拦截器参数部分，如果允许访问返回true，否则false；
+     * @param servletRequest
+     * @param servletResponse
+     * @param o
+     * @return
+     * @throws Exception
+     */
     protected boolean isAccessAllowed(javax.servlet.ServletRequest servletRequest, javax.servlet.ServletResponse servletResponse, Object o) throws Exception {
         Object token = SecurityUtils.getSubject().getPrincipal();
         if (null != token || isLoginRequest(servletRequest, servletResponse)) {// && isEnabled()
@@ -22,6 +30,13 @@ public class MyShiroPermFilter extends AccessControlFilter {
         return Boolean.FALSE;
     }
 
+    /**
+     * 表示访问拒绝时是否自己处理，如果返回true表示自己不处理且继续拦截器链执行，返回false表示自己已经处理了（比如重定向到另一个页面）
+     * @param servletRequest
+     * @param servletResponse
+     * @return
+     * @throws Exception
+     */
     protected boolean onAccessDenied(ServletRequest servletRequest, ServletResponse servletResponse) throws Exception {
         //保存Request和Response 到登录后的链接
         saveRequestAndRedirectToLogin(servletRequest, servletResponse);
