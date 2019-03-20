249ffe052622d79591ccda98c8fbccacce611ad0
snail <lt-2046@163.com>
2019-03-20 15:14:49 +0800

    EC缓存修改

--- snail-web/src/main/java/com/snail/shiro/session/CustomEcSessionManager.java
+++ snail-web/src/main/java/com/snail/shiro/session/CustomEcSessionManager.java
@@ -3,6 +3,7 @@ package com.snail.shiro.session;
 import com.snail.shiro.cache.ShiroECCacheManager;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.shiro.cache.Cache;
 import org.apache.shiro.session.Session;
 import org.apache.shiro.session.mgt.eis.EnterpriseCacheSessionDAO;
 import org.apache.shiro.subject.SimplePrincipalCollection;
@@ -15,6 +16,14 @@ import java.util.List;
  * Created by liutao on 2019/1/1.
  */
 public class CustomEcSessionManager {
+    /**
+     * 全局无超时缓存
+     */
+    public static final String GLOBAL_CACHE="defaultCache";
+    /**
+     * session缓存
+     */
+    public static final String SESSION_CACHE="activeSessionCache";
 
     Logger logger = LogManager.getLogger(CustomEcSessionManager.class.getName());
     private ShiroECCacheManager shiroECCacheManager;
@@ -36,7 +45,22 @@ public class CustomEcSessionManager {
         this.enterpriseCacheSessionDAO = enterpriseCacheSessionDAO;
     }
 
+    public void putCache(String cacheName,String key ,Object value){
+        getCache(cacheName).put(key,value);
+    }
+
+    public Object getCacheValue(String cacheName,String key){
+        return getCache(cacheName).get(key);
+    }
+
+    private Cache getCache(String cacheName){
+        Cache cache = shiroECCacheManager.getCache(cacheName);
+        return cache;
+    }
+
     public List getAllUser(){
+
+
         Collection<Session> allSession = enterpriseCacheSessionDAO.getActiveSessions();
         if(allSession==null){
             return null;
